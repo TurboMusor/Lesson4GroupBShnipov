32d50a0e40e61aced2bf402fe7a30bee5ea73b55
Yakov Shnipov <shnipov@gmail.com>
2023-08-06 14:34:09 +0300

    third message

--- Src/ProgramLesson4.java
+++ Src/ProgramLesson4.java
@@ -3,5 +3,6 @@ public class ProgramLesson4 {
     public static void main(String[] args){
         System.out.println("Hello git");
         System.out.println("hello git from Yakov");
+        System.out.println("Hello git fron our group");
     }
 }
